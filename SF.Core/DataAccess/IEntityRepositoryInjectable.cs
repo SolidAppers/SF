2ca55fa0faa14d49ade781b6d1a85212cb4d7f43
mkavici <mkavici@outlook.com>
2024-12-08 19:46:33 +0300

    Injectable repository

---
+++ SF.Core/DataAccess/IEntityRepositoryInjectable.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using SF.Core.Entities;
+using SF.Core.Utilities.Results;
+
+namespace SF.Core.DataAccess
+{
+    public interface IEntityRepositoryInjectable<T>
+        where T : class, IEntity
+    {
+        T Add(T entity);
+        T Update(T entity);
+        void Delete(T entity);
+        IEnumerable<T> GetList(Expression<Func<T, bool>> expression = null);
+        Task<IEnumerable<T>> GetListAsync(Expression<Func<T, bool>> expression = null);
+       T Get(Expression<Func<T, bool>> expression);
+        Task<T> GetAsync(Expression<Func<T, bool>> expression);
+        int SaveChanges();
+        Task<int> SaveChangesAsync();
+        IQueryable<T> Query();
+        Task<int> Execute(FormattableString interpolatedQueryString);
+
+        TResult InTransaction<TResult>(Func<TResult> action, Action successAction = null, Action<Exception> exceptionAction = null);
+
+        Task<int> GetCountAsync(Expression<Func<T, bool>> expression = null);
+        int GetCount(Expression<Func<T, bool>> expression = null);
+    }
+}
